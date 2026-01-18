a5311458296c8f14586ed796c898f2c509dc5bea
seb pj <pj.sebastien@gmail.com>
2026-01-18 10:50:03 +0400

    Add 301 redirect from /fr to homepage
    - Redirect /fr to /
    - Redirect /fr/* to /*
    - Permanent redirect (301) for SEO
    - Compatible with Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/fr',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/fr/:path*',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
